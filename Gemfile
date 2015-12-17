ba16041d10b0e6f2e12be2f29375e47b245dd44c
sue445 <sue445@sue445.net>
2015-12-17 09:46:03 +0900

    bundle gem itamae-plugin-recipe-git_now --mit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+# Specify your gem's dependencies in itamae-plugin-recipe-git_now.gemspec
+gemspec
---
+++ lib/itamae/plugin/recipe/git_now.rb
@@ -0,0 +1,11 @@
+require "itamae/plugin/recipe/git_now/version"
+
+module Itamae
+  module Plugin
+    module Recipe
+      module GitNow
+        # Your code goes here...
+      end
+    end
+  end
+end
